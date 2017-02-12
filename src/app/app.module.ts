d49c5fa755bb0fcd74c16d865ffd4d9f3b87733b
Lasse <lasse.linkola@student.tut.fi>
2017-02-12 15:08:03 +0200

    Project setup with angluar-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-
+import { AlertModule } from 'ng2-bootstrap';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot(rootRouterConfig, {useHash: true}) 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'navbar',
+    styleUrls:['navbar.component.css'],
+    templateUrl: './navbar.component.html'
+})
+
+export class NavbarComponent{
+}
